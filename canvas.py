bd6c58e2430aee69e7bb8272fb93c1f55e62f9fa
Carter Heron <35495189+heronc95@users.noreply.github.com>
2018-04-20 13:04:06 -0400

    Add files via upload
    Now uploads as well as downloads

--- canvas.py
+++ canvas.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
 
     canvas_key = auth.getcanvas_key()
     canvas_url = auth.getcanvas_url()
-
+    download_filename = 'test2.pdf'
     # Set up a session
     session = requests.Session()
     session.headers = {'Authorization': 'Bearer %s' % canvas_key}
@@ -20,6 +20,7 @@ if __name__ == '__main__':
     r = r.json()
     print(' ')
     print(r)  # This successfully returns the expected response...
+
     # Step 2 - upload file
     payload = list(r['upload_params'].items())  # Note this is now a list of tuples
     print(' ')
@@ -32,3 +33,10 @@ if __name__ == '__main__':
     r = r.json()  # The requests now works and returns response 200 - not 301.
     print(' ')
     print(r)  # This is a dictionary containing some info about the uploaded file
+
+    # Step 3 - download file
+    r = requests.get(canvas_url+'/'+download_filename, allow_redirects=True)
+    open(download_filename, 'wb').write(r.content)
+    r = r.json()  # The requests now works and returns response 200 - not 301.
+    print(' ')
+    print(r)  # This is a dictionary containing some info about the downloaded file
