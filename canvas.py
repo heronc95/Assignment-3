6bfd1a0f165df7fb31aac1d5e0578ae337113e7a
Carter Heron <35495189+heronc95@users.noreply.github.com>
2018-04-20 13:07:05 -0400

    uploads and downloads files to canvas group page

--- canvas.py
+++ canvas.py
@@ -8,12 +8,13 @@ if __name__ == '__main__':
     canvas_key = auth.getcanvas_key()
     canvas_url = auth.getcanvas_url()
     download_filename = 'test2.pdf'
+    upload_filename   = 'test.pdf'
     # Set up a session
     session = requests.Session()
     session.headers = {'Authorization': 'Bearer %s' % canvas_key}
     # Step 1 - tell Canvas you want to upload a file
     payload = {}
-    payload['name'] = 'test.pdf'
+    payload['name'] = upload_filename
     payload['parent_folder_path'] = '/'
     r = session.post(canvas_url, data=payload)
     r.raise_for_status()
@@ -25,7 +26,7 @@ if __name__ == '__main__':
     payload = list(r['upload_params'].items())  # Note this is now a list of tuples
     print(' ')
     print(payload)
-    with open('test.pdf', 'rb') as f:
+    with open(upload_filename, 'rb') as f:
         file_content = f.read()
     payload.append((u'file', file_content))  # Append file at the end of list of tuples
     r = requests.post(r['upload_url'], files=payload)
