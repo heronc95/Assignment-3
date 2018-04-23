bcb1debfddf7294096486c4b1d1a7b7f7690e956
Carter Heron <35495189+heronc95@users.noreply.github.com>
2018-04-22 21:52:04 -0400

    download works

--- canvas.py
+++ canvas.py
@@ -1,17 +1,18 @@
 from canvas_token import authentication
 import requests
 import json
-
+import wget
 if __name__ == '__main__':
     auth = authentication()
 
     canvas_key = auth.getcanvas_key()
     canvas_url = auth.getcanvas_url()
-    download_filename = 'test2.pdf'
-    upload_filename   = 'test.pdf'
+    download_filename = 'hello2.txt'
+    upload_filename   = 'hello.txt'
     # Set up a session
     session = requests.Session()
     session.headers = {'Authorization': 'Bearer %s' % canvas_key}
+
 
     # Step 1 - tell Canvas you want to upload a file
     payload = {}
@@ -22,6 +23,7 @@ if __name__ == '__main__':
     r = r.json()
     print(' ')
     print(r)  # This successfully returns the expected response...
+
 
     # Step 2 - upload file
     payload = list(r['upload_params'].items())  # Note this is now a list of tuples
@@ -36,16 +38,34 @@ if __name__ == '__main__':
     print(' ')
     print(r)  # This is a dictionary containing some info about the uploaded file
 
+
     # Step 3 - download file
-    r = requests.get(canvas_url+'/'+download_filename, allow_redirects=True)
-    open(download_filename, 'wb').write(r.content)
-    r = r.json()  # The requests now works and returns response 200 - not 301.
+    params = (
+        ('sort=name')
+    )
+    session.headers = {'Authorization': 'Bearer %s' % canvas_key}
+    r = session.get(canvas_url+'?'+'only[]=names',params=params)
+    r.raise_for_status()
+    r = r.json()
+    for x in r:
+        if (x['filename']) == download_filename:
+            file_id = (x['id'])
+    r = session.get(canvas_url+'/'+str(file_id), stream=True)
+    r = r.json()
+    with open(download_filename, 'wb') as f:
+        f.write(file_content)
+
     print(' ')
     print(r)  # This is a dictionary containing some info about the downloaded file
 
+
     # Step 4 - list the files
-    r = session.get(canvas_url)
+    params = (
+        ('sort=name')
+    )
+    r = session.get(canvas_url+'?'+'only[]=names',params=params)
     r.raise_for_status()
     r = r.json()
-    print(' ')
-    print(r)
+    print('\nFiles within folder:')
+    for x in r:
+        print(x['filename'])
